bb7e44ab4908e63f269a6801a3ae81b28c98eda0
Matos <gmatosalbuquerque.38@gmail.com>
2021-12-21 16:52:42 -0300

    Composition

--- src/application/program_composition.java
+++ src/application/program_composition.java
@@ -1,6 +1,37 @@
 package application;
 
+import entitiesComposition.departament;
+import entitiesComposition.worker;
+import entitiesEnum.workLevel;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class program_composition {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com o departamento:");
+        String departamentName = sc.nextLine();
+        System.out.println("Entre com os dados do trabalhador:");
+        System.out.println("name:");
+        String name =sc.nextLine();
+        System.out.println("Level:");
+        String workerLevel = sc.nextLine();
+        System.out.println("Salário:");
+        double baseSalary =sc.nextDouble();
+
+        worker worker = new worker(name, workLevel.valueOf(workerLevel), baseSalary, new departament(departamentName) );
+
+        System.out.println(worker);
+
+
+
+        sc.close();
+
+
+
     }
 }
